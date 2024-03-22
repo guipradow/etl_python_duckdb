b941d235b75236af041931ea197135a0c91f1172
Guilherme Prado <gui_prado@outlook.com>
2024-03-21 23:38:50 -0300

    corrige baixar_pasta_google_drive

--- pipeline_01.py
+++ pipeline_01.py
@@ -75,7 +75,7 @@ def pipeline():
     url_pasta = 'https://drive.google.com/drive/folders/19flL9P8UV9aSu4iQtM6Ymv-77VtFcECP'
     diretorio_local = './pasta_gdown'
 
-    # baixar_pasta_google_drive(url_pasta, diretorio_local)
+    baixar_pasta_google_drive(url_pasta, diretorio_local)
     con = conectar_banco()
     inicializar_tabela(con)
     processados = arquivos_processados(con)
