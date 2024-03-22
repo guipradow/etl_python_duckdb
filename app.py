47dcbda19ba31d26f90fca8720441e7e29ba1d37
Guilherme Prado <gui_prado@outlook.com>
2024-03-21 23:14:41 -0300

    deploy

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from pipeline_01 import pipeline
 st.title('Processador de Arquivos')
 
 if st.button('Processar'):
-  with st.spinner('Processando...'):
-    logs = pipeline()
-    # Exibe os logs no streamlit
-    for log in logs:
-      st.write(log)
+    with st.spinner('Processando...'):
+        logs = pipeline()
+        # Exibe os logs no streamlit
+        for log in logs:
+            st.write(log)
